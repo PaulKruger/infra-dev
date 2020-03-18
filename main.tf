2b27fdc225447541a919d8f0d42b3e116e0e9122
PaulKruger <paul.kruger@pskruger.com>
2020-03-18 16:52:21 -0400

    Saving State

--- main.tf
+++ main.tf
@@ -1,3 +1,44 @@
+# Variables
+variable "location" {
+  default     = "eastus"
+  description = "Region in which the resources will be deployed"
+}
+
+variable "az_resource_group" {
+  default     = "TAFiDevStack"
+  description = "Resource Group in Azure to add resources to"
+}
+
+variable "az_subscription_id" {
+  default = "5c10115a-819f-428b-b7a9-d607a2951d2b"
+  description = "The subscription identifier."
+}
+
+variable "tafi_vpn_address" {
+  default = "35.232.216.163"
+  description = "VPN Provided by TAFI"
+}
+
+variable "address_prefix" {
+  default     = "10.10.0.0/24"
+  description = "Subnet address prefix"
+}
+
+variable "firewall_subnet_prefix" {
+  default     = "10.10.1.0/24"
+  description = "Subnet address prefix"
+}
+
+variable "gke_username" {
+  default     = "p.kruger@craneware.com"
+  description = "gke username"
+}
+
+variable "gke_password" {
+  default     = "thisIz!taf1_Pw@!"
+  description = "gke password"
+}
+
 # main teraform code
 #Azure Setup
 terraform {
