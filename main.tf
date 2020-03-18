62ff475e62c5ae38a3fb885b38fe6c0522e80efa
PaulKruger <paul.kruger@pskruger.com>
2020-03-18 16:51:34 -0400

    Saving State

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 # main teraform code
 #Azure Setup
 terraform {
-  backend "terraform" {
+  backend "azurerm" {
     resource_group_name   = "TAFiDevStack"
     storage_account_name  = "tafiterraformdev2"
-    container_name        = "tafiterraformdev"
+    container_name        = "tafiterraformstatedev"
     key                   = "znLXpx2j4hDqXtDPcAzfChvUO20wA5mgqj84Ab6uo9+ze2NPnie3VjyMwI2XZMKF50XbOv6cu452CjB54PIWxg=="
   }
 }
